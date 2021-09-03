ffb65802cd280d913fa4b0365fd9ad7069cafe07
chukmas20 <74096185+chukmas20@users.noreply.github.com>
2021-09-03 08:40:09 +0100

    auth update

---
+++ backend/models/userModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema({
+
+   name:{
+       type:String,
+       required:true
+   },
+   email:{
+    type:String,
+    required:true,
+    unique:true,
+},
+password:{
+    type:String,
+    required:true
+},
+isAdmin:{
+    type:Boolean,
+    default:false
+},
+pic:{
+    type:String,
+    required:true,
+    default:"https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/71/071396/1.jpg?2974"
+},
+
+},{
+    timestamps:true
+}
+);
+
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password")){
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+})
+
+module.exports = mongoose.model("User", userSchema);
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoute");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 
 dotenv.config();
 connectDB();
+
+app.use(express.json());
 
 app.get("/", (req,res)=>{
    res.send("Api is Running");
@@ -16,10 +20,10 @@ app.get("/api/notes", (req,res)=>{
     res.json(notes);
  });
 
- app.get("/api/notes/:id", (req,res)=>{
-     const note = notes.find((n)=> n._id === req.params.id);
-     res.send(note);
- });
+ app.use("/api/users", userRoutes)
+
+//  app.use(notFound);
+//  app.use(errorHandler);
 
  const PORT = process.env.PORT || 5000;
 
